eb3ba3be1f8f559e338673fc82b70f2de71f29d1
Long Nguyen <hplnguyen@absolute.com>
2019-05-06 17:44:26 +0700

    Add Interfaces lession

---
+++ interfaces/emptyinterfaces.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func processMap(parentPath string, m map[string]interface{}) []map[string]interface{} {
+	var queue []map[string]interface{}
+	for k, v := range m {
+		queue = append(queue, map[string]interface{}{parentPath + k : v})
+	}
+
+	return queue
+}
+
+func processSlice(parentPath string, arr []interface{}) []map[string]interface{} {
+	var queue []map[string]interface{}
+	path := parentPath[:len(parentPath) - 1]
+	for i, e := range arr {
+		queue = append(queue, map[string]interface{}{fmt.Sprintf("%s[%d]", path, i) : e})
+	}
+
+	return queue
+}
+
+func Flatten(m map[string]interface{}, sep string) map[string]interface{} {
+	ret := make(map[string]interface{})
+
+	queue := processMap("", m)
+
+	for len(queue) > 0 {
+		node := queue[0]
+		queue = queue[1:]
+
+		// the map taken from queue node has only 1 key and 1 value:
+		// the key is the path to the value
+		for k, v := range node {
+			switch vt := v.(type) {
+			case map[string]interface{}:
+				queue = append(queue, processMap(k + sep, vt)...)
+			case []interface{}:
+				queue = append(queue, processSlice(k + sep, vt)...)
+			default:
+				ret[k] = v
+			}
+		}
+	}
+
+	return ret
+}
+
+func main() {
+	jsonObj := map[string]interface{} {
+		"sub1": map[string]interface{} {
+			// Must be "map[string]interface{}" instead of "map[string]int"
+			// otherwise "case map[string]interface{}" will fail
+			"sub11": map[string]interface{} {
+				"sub111": 111,
+				"sub112": 112,
+			},
+			"sub12": map[string]interface{} {
+				"sub121": []interface{} {"121_1", "121_2"},
+			},
+		},
+		"sub2": map[string]interface{} {
+			"sub21": time.Now(),
+			"sub22": []interface{} {
+				map[string]interface{} {
+					"sub221": 10,
+					"sub222": "222",
+				},
+				"223",
+				nil,
+			},
+		},
+	}
+
+	fmt.Println(jsonObj)
+	flattened := Flatten(jsonObj, ".")
+	fmt.Println(flattened)
+}
---
+++ interfaces/go.mod
@@ -0,0 +1,3 @@
+module github.com/philongct/golessions/interfaces
+
+go 1.12
