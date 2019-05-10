96539cbe8a5182d5b80a69e55f38f601453a3b32
Long Nguyen <hplnguyen@absolute.com>
2019-05-10 10:26:58 +0700

    Add some docs

--- receiverandpointer/receiverandpointer.go
+++ receiverandpointer/receiverandpointer.go
@@ -4,10 +4,12 @@ type Object struct {
 	Attr string
 }
 
+// Pass by value doesn't retain changes
 func (o Object) SetAttr(value string) {
 	o.Attr = value
 }
 
+// Pass by reference retains changes
 func (o *Object) ReplaceAttr(newValue string) string {
 	ret := o.Attr
 	o.Attr = newValue
--- receiverandpointer/receiverandpointer_test.go
+++ receiverandpointer/receiverandpointer_test.go
@@ -1,11 +1,13 @@
 package receiverandpointer
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestReceiverAndPointer1(t *testing.T) {
+	// Even o is a pointer, attribute remained unchanged if receiver is not pointer
+	// NOTE: non-pointer receiver work for both pointer and non-pointer
 	o := &Object{"attr1"}
 	o.SetAttr("abc")
 	// output: &{attr1}    {attr1}
@@ -20,6 +22,8 @@ func TestReceiverAndPointer2(t *testing.T) {
 }
 
 func TestReceiverAndPointer3(t *testing.T) {
+	// Because receiver is pointer, attribute change will retain for non-pointer
+	// Pointer receiver accept both pointer and non-pointer
 	o := Object{"attr"}
 	// output: attr    {newAttr}
 	fmt.Print(o.ReplaceAttr("newAttr"), "\t")
@@ -27,6 +31,8 @@ func TestReceiverAndPointer3(t *testing.T) {
 }
 
 func TestReceiverAndPointer4(t *testing.T) {
+	// Attribute change will retained for pointer
+	// Pointer receiver accept both pointer and non-pointer
 	o := &Object{"attr"}
 	o.ReplaceAttr("newAttr")
 	// output: &{newAttr}       {newAttr}
